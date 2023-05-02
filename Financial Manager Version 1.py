4062a2a78710c406fdce2b5221d723de3807f85d
Kashyap Sukshavasi <101945096+Kashyap-Sukshavasi@users.noreply.github.com>
2023-05-01 22:16:48 -0400

    Update Financial Manager Version 1.py
    For this code update, I have added the code for the middle class financial manager. I have to planned to create a function which contains all the classes and their managers, so that once the tutorial ( line 43 ) has finished the code will automatically redirect to line 4.

--- Financial Manager Version 1.py
+++ Financial Manager Version 1.py
@@ -3,12 +3,14 @@ question = input("Is this your first time using my financial manager?").lower()
 if question=="no":
     answer = input("What stage are you in your financial journey?\nJust to remind you, there are 3 levels. Low Income, Middle Class, and High Income!").title()
     if answer == "Low Income":
+        #All the variables listed here as well as the calculations for them.
         budget_poor = float(input("What is your budget every month?"))
         groceries_poor = 0.1 * budget_poor
         emergency_poor = 0.05 * budget_poor
         clothing_poor = 0.1 * budget_poor
         rent_poor = 0.5 * budget_poor
         savings_poor = 0.25 * budget_poor
+        #All the data given above using the budget to be displayed visually below using print statements.
         print("\n\n***************************************************")
         print(f"\nYour monthly budget overall is ${budget_poor}.")
         print(f"Your monthly recommended budget for groceries is ${groceries_poor}")
@@ -17,14 +19,30 @@ if question=="no":
         print(f"Your monthly recommended budget for rent is ${rent_poor}")
         print(f"Your monthly recommended budget for savings is ${savings_poor}")
         exit()
-    if answer == "middle class":
-        
-    if answer == "high income":
-        
-    else:
-        #Main place for initial code launch Ex:Greetings, run code, etc.
-        print("Welcome to the All Class Financial Manager!\nCreated by Kashyap Sukshavasi")
-        print("\nThere are 3 stages to this financial manager.")
-        print("\nFirstly, Low Income. This is if your monthly pay is below $1,500")
-        print("\nSecond, Middle Class. This is if your monthly pay is between $3,000 and $10,000.")
-        print("\nFinally, High Income. This is if your monthly pay is higher that $20,000.")
+    if answer == "Middle Class":
+        #All the variables listed here as well as the calculations for them.
+        budget_middle = float(input("What is your budget every month?"))
+        groceries_middle = 0.15 * budget_middle
+        emergency_middle = 0.05 * budget_middle
+        clothing_middle = 0.15 * budget_middle
+        rent_middle = 0.4 * budget_middle
+        savings_middle = 0.25 * budget_middle
+        #All the data given above using the budget to be displayed visually below using print statements.
+        print("\n\n***************************************************")
+        print(f"\nYour monthly budget overall is ${budget_middle}.")
+        print(f"Your monthly recommended budget for groceries is ${groceries_middle}")
+        print(f"Your monthly recommended budget for emergencies is ${emergency_middle}")
+        print(f"Your monthly recommended budget for clothing is ${clothing_middle}")
+        print(f"Your monthly recommended budget for rent is ${rent_middle}")
+        print(f"Your monthly recommended budget for savings is ${savings_middle}")
+        exit()
+    if answer == "High Income":
+        print('This is just a filler, future code will fix this statement.')
+        #This is just a filler to not have any problems with the else code on line 38
+else:
+    #Main place for initial code launch Ex:Greetings, run code, etc.
+    print("Welcome to the All Class Financial Manager!\nCreated by Kashyap Sukshavasi")
+    print("\nThere are 3 stages to this financial manager.")
+    print("\nFirstly, Low Income. This is if your monthly pay is below $1,500")
+    print("\nSecond, Middle Class. This is if your monthly pay is between $3,000 and $10,000.")
+    print("\nFinally, High Income. This is if your monthly pay is higher that $20,000.")
