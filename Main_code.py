2ce733183d5a6d8f04aac0e8f7fba5371cf11f60
Kashyap Sukshavasi <101945096+Kashyap-Sukshavasi@users.noreply.github.com>
2023-05-02 21:46:57 -0400

    Add files via upload
    This is the main code for the whole project, this will be used a startup page and a collective place to host all the modes of money managers.

---
+++ Main_code.py
@@ -0,0 +1,30 @@
+#Importing all the modules necessary to create a user interface with all the managers
+import os
+import time
+import math
+from Budget_manager  import main_code_budget
+
+print("Welcome!")
+print("\nThis is the Money Manager Application built for you. This app is brought to you by Kashyap Sukshavasi(Lead Programmer) and Zaid Soboh(Lead Programmer)!")
+tutorial_question = input("\n\nIs this your first time using our application [yes/no]?:").title()
+
+if tutorial_question == "Yes":
+    print("\n\n********************************************")
+    time.sleep(2)
+    print("Loading tutorial...25%")
+    time.sleep(4)
+    print("Loading tutorial...75%")
+    time.sleep(1)
+    print("Loading tutorial...100%")
+    time.sleep(2)
+    print("Welcome to your tutorial!")
+    print("\n\n")
+elif tutorial_question == "No":
+    print("\n\nYou have multiple modes to choose from, such as...")
+    print("\n1 - Monthly Budget Manager")
+    print("\n2 - Future stuff...")
+    print("\n\n")
+    time.sleep(3)
+    modes_question = input("Which mode would you like to use today?\nUse the numbers associated with the mode as an answer!\nYour answer:")
+    if modes_question == "1":
+        main_code_budget()
