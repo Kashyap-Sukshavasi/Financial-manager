a3d66cc6120ee7af34d49b561dcf6c24606f8d57
Kashyap Sukshavasi <101945096+Kashyap-Sukshavasi@users.noreply.github.com>
2023-05-03 00:35:37 -0400

    Update Fixed_deposit.py

--- Fixed_deposit.py
+++ Fixed_deposit.py
@@ -1,3 +1,16 @@
+def run_deposit():
+    print("\n\nThis is a fixed deposit calculator.")
+    principle = float(input("What is your principle amount in your local currency?: "))
+    interest = float(input("What is the interest rate for your fixed deposit.\nNote: Give the value in the numerical percentage %\nWhat is your ineterest?: "))
+    time_year = float(input("What is the time for your fixed deposit in years?: "))
+    i_money = principle * (interest/100) * time_year
+    p_money = i_money + principle
+    print(f"\n\nYour principle amount is ${principle} in your local currency.")
+    print(f"Your interest rate in percentages is {interest}%.")
+    print(f"Your time for the fixed deposit is {time_year} years.")
+    print(f"The money you will get for interest is ${i_money} in your local currency.")
+    print(f"Your total money including all charges is ${p_money} in your local currency.")      
+
 def main_code_deposit():
     #Importing all the libraries needed for this project
     import os
@@ -8,15 +21,6 @@ def main_code_deposit():
     question1 = input("Is this your first time using my budget manager [yes/no]?: ").title()
 
     if question1 == "No":
-        print("\n\nThis is a fixed deposit calculator.")
-        principle = float(input("What is your principle amount in your local currency?: "))
-        interest = float(input("What is the interest rate for your fixed deposit.\nNote: Give the value in the numerical percentage %\nWhat is your ineterest?: "))
-        time_year = float(input("What is the time for your fixed deposit in years?: "))
-        i_money = principle * (interest/100) * time_year
-        p_money = i_money + principle
-        print(f"Your principle amount is ${principle} in your local currency.")
-        print(f"Your interest rate in percentages is {interest}%.")
-        print(f"Your time for the fixed deposit in years is {time_year} years.")
-        print(f"The money you will get for interest is ${i_money} in your local currency.")
-        print(f"Your total money including all charges ${p_money} in your local currency.")
-main_code_deposit()
+        run_deposit()
+if __name__ == "__main__":
+    main_code_deposit()
